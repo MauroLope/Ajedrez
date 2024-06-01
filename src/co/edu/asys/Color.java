2ea7b2187068fa694aec1c6af31181454e6fde92
Mauricio López <mauriciolopezosorio99@gmail.com>
2024-06-01 12:16:02 -0500

    fist commit

---
+++ src/co/edu/asys/Color.java
@@ -0,0 +1,5 @@
+package co.edu.asys;
+
+public enum Color {
+    BLANCO,NEGRO
+}
---
+++ src/co/edu/asys/Ficha.java
@@ -0,0 +1,15 @@
+package co.edu.asys;
+
+
+public abstract class Ficha {
+    protected Color color;
+    public Ficha(Color color) {
+        this.color = color;
+    }
+
+
+
+    public void mover() {
+    }
+
+}
